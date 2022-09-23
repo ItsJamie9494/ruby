5508c31255666150fb33e1a6ee0e621a4bb1fec6
Jamie Murphy <hello@itsjamie.dev>
2022-09-22 22:10:59 -0700

    Initial Commit

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+//! # Ruby
+//! A crate for installing Linux distributions
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
